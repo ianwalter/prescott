b2a589766f0c6de087639c29fa87d171e0792f61
Ian Walter <public@iankwalter.com>
2018-01-25 11:13:11 -0500

    Fixed Handlebars nested value

--- src/prescott.js
+++ src/prescott.js
@@ -1,34 +1,68 @@
 const { parse } = require('./parser')
 
+function appendOutput (output, tokens, data) {
+  for (const token of tokens) {
+    if (token.type === 'Text') {
+      output += token.content
+    } else if (token.type === 'Handlebars') {
+      let value
+      for (const namespace of token.value.split('.')) {
+        if (value) {
+          value = value[namespace]
+        } else {
+          value = data[namespace]
+        }
+      }
+      output += value
+    } else if (token.type === 'SelfClosingTag') {
+      if (token.name === 'n') {
+        output += '\n'
+      }
+    } else if (token.type === 'BalancedTag') {
+      if (token.name === 'iterate') {
+
+        // //
+        // const over = token.attributes.find(a => a.name === 'over')
+        // let collection = data[over]
+        // if (collection instanceof Array) {
+        //   collection = collection.map((v, i) => [i, v])
+        // } else if (collection instanceof Object) {
+        //   collection = Object.entries(data[over])
+        // } else if (collection instanceof Map) {
+        //   collection = Array.from(collection)
+        // } else {
+        //   // TODO throw error
+        // }
+
+        // //
+        // const key = token.attributes.find(a => a.name === 'key')
+        // const value = token.attributes.find(a => a.name === 'value')
+        // for (const [i, v] of collection) {
+        //   const localData = clone(data)
+        //   if (key) {
+        //     localData[key] = i
+        //   }
+        //   if (value) {
+        //     localData[value] = v
+        //   }
+        //   output = appendOutput(output, token.content, localData)
+        // }
+      } else if (token.name === 'if') {
+
+      } else if (token.name === 'else') {
+
+      }
+    }
+  }
+
+  return output
+}
+
 module.exports = {
   compile: template => {
     // Parse the given template using the Peg.js parser.
-    const parsedTemplate = parse(template)
+    const tokens = parse(template)
 
-    return data => {
-      let output = ''
-
-      for (const token of parsedTemplate) {
-        if (token.type === 'Text') {
-          output += token.content
-        } else if (token.type === 'Handlebars') {
-          output += data[token.value]
-        } else if (token.type === 'SelfClosingTag') {
-          if (token.name === 'n') {
-            output += '\n'
-          }
-        } else if (token.type === 'BalancedTag') {
-          if (token.name === 'iterate') {
-
-          } else if (token.name === 'if') {
-
-          } else if (token.name === 'else') {
-
-          }
-        }
-      }
-
-      return output
-    }
+    return data => appendOutput('', tokens, data)
   }
 }
--- tests/parser.test.js
+++ tests/parser.test.js
@@ -7,12 +7,12 @@ const output = parse(template)
 
 describe('Parser', () => {
   test('parses a Handlebars type correctly', () => {
-    const handlebars = output.filter(t => t.type === 'Handlebars')[0]
+    const handlebars = output.find(t => t.type === 'Handlebars')
     expect(handlebars.value).toBe('name')
   })
 
   test('parses a iterate Tag correctly', () => {
-    const iterate = output.filter(t => t.name === 'iterate')[0]
+    const iterate = output.find(t => t.name === 'iterate')
     expect(iterate.attributes).toContainEqual({ name: 'over', value: 'badges' })
     expect(iterate.attributes).toContainEqual({ name: 'value', value: 'badge' })
     expect(iterate.content.length).toBeGreaterThan(0)
